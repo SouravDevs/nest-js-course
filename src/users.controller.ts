7ee3e5c01c2a9bf0a7cf5b527b4979b2d6307d2d
SouravDevs <sourav2x4x@gmail.com>
2026-02-09 23:32:47 +0530

    Body Parameters

--- src/users.controller.ts
+++ src/users.controller.ts
@@ -1,27 +1,16 @@
-import { Controller, Get, Param, Query, Headers } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 
-interface VideoParams {
+interface VideoDTO {
     id: number;
-    name: string
-}
-
-interface VideoQuery {
-    id: number;
-    name: string
+    name: string;
 }
 
 @Controller("/users")
 export class UsersController {
 
-    @Get("/videos/:id/:name")
-    getVideos(@Param() params: VideoParams) {
-        console.log(params.id, params.name)
-        return "success"
-    }
-
-    @Get("/videos")
-    getPhotos(@Query() query: VideoQuery, @Headers("hii") headers: Record<string, any>) {
-        console.log(headers)
-        return "success"
+    @Post("/video")
+    addVideo(@Body("name") body: VideoDTO) {
+        const { id, name } = body;
+        return { success: { id, name} }
     }
 }
