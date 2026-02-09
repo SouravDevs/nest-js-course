065b3cb7c39d563e8497076bac65dc77bc3fbf3d
SouravDevs <sourav2x4x@gmail.com>
2026-02-10 00:07:41 +0530

    User Resouces Examples

--- src/users.controller.ts
+++ src/users.controller.ts
@@ -1,16 +1,55 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 
-interface VideoDTO {
-    id: number;
+
+interface CreateUserDTO {
     name: string;
+    age: number;
+    id: string;
 }
+
+let users: CreateUserDTO[] = []
 
 @Controller("/users")
 export class UsersController {
 
-    @Post("/video")
-    addVideo(@Body("name") body: VideoDTO) {
-        const { id, name } = body;
-        return { success: { id, name} }
+    @Post()
+    addUser(@Body() createUserDto: CreateUserDTO) {
+        users.push(createUserDto)
+        console.log(users)
+        return "user added"
+    }
+
+    @Get()
+    getUsers() {
+        return users
+    }
+
+    @Get(':id')
+    getUser(@Param("id") id: number) {
+        const user = users.find((user) => +user.id === +id)
+
+        if(!user) {
+            return "No user found"
+        }
+        return user
+    }
+
+    @Put(":id")
+    updateUser(@Param("id") id: number, @Body() updateUserDTO: CreateUserDTO) {
+        const userIdx = users.findIndex((user) => +user.id === +id)
+
+        if(!userIdx) {
+            return
+        }
+        else {
+            users[userIdx] = updateUserDTO
+            return "User updated"
+        }
+    }
+
+    @Delete(":id")
+    deleteUser(@Param("id") id: number) {
+        users = users.filter((user) => +user.id === +id)
+        return "User deleted"
     }
 }
