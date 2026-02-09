ce3b99055c54e3de34a3b22651581588275d3ab0
SouravDevs <sourav2x4x@gmail.com>
2026-02-10 00:12:41 +0530

    Sub-Domain Routing | Host & IP Details

--- src/users.controller.ts
+++ src/users.controller.ts
@@ -1,55 +1,12 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { Controller, Get, Ip } from "@nestjs/common";
 
-
-interface CreateUserDTO {
-    name: string;
-    age: number;
-    id: string;
-}
-
-let users: CreateUserDTO[] = []
-
-@Controller("/users")
+@Controller('/users')
 export class UsersController {
 
-    @Post()
-    addUser(@Body() createUserDto: CreateUserDTO) {
-        users.push(createUserDto)
-        console.log(users)
-        return "user added"
-    }
-
     @Get()
-    getUsers() {
-        return users
+    getIP(@Ip() ip: string) {
+        console.log(ip)
+        return 'success'
     }
-
-    @Get(':id')
-    getUser(@Param("id") id: number) {
-        const user = users.find((user) => +user.id === +id)
-
-        if(!user) {
-            return "No user found"
-        }
-        return user
-    }
-
-    @Put(":id")
-    updateUser(@Param("id") id: number, @Body() updateUserDTO: CreateUserDTO) {
-        const userIdx = users.findIndex((user) => +user.id === +id)
-
-        if(!userIdx) {
-            return
-        }
-        else {
-            users[userIdx] = updateUserDTO
-            return "User updated"
-        }
-    }
-
-    @Delete(":id")
-    deleteUser(@Param("id") id: number) {
-        users = users.filter((user) => +user.id === +id)
-        return "User deleted"
-    }
+   
 }
