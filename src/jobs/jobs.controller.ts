78a226e05fb34ccb9382527f835513cb9b50b8d9
SouravDevs <sourav2x4x@gmail.com>
2026-02-13 01:17:13 +0530

    #21- Pipes | Built-in Validation Pipes | Enum, UUID & Arrays

--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, DefaultValuePipe, Get, Param, ParseBoolPipe, ParseFloatPipe, ParseIntPipe, Put, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseArrayPipe, ParseEnumPipe, ParseUUIDPipe, Put, Query } from '@nestjs/common';
 
 enum JobType {
     FULL_TIME,
@@ -8,48 +8,45 @@ enum JobType {
 @Controller('jobs')
 export class JobsController {
 
-    @Get(":id")
-    findJobById(@Param("id", ParseIntPipe) id: number) {
-        console.log(typeof id)
-        return "Success"
+    @Get("search")
+    findJobsById(@Query("id", new ParseArrayPipe({ items: Number, separator: "-"})) ids: number[]) {
+        console.log(ids)
+        return ids
     }
 
-    // Explicitly set status code
-    // findJobById(@Param("id", new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE})) id: number) {
-    //     console.log(typeof id)
-    //     return "Success"
-    // }
+    @Get(":id")
+    findJobById(@Param("id",) id: number) {
+
+    }
 
     @Put("salary/:id")
-    // @UsePipes(ParseIntPipe)
-    incSalary(@Param("id", ParseIntPipe) id: number, @Query("increment", ParseFloatPipe) inc: number) {
-        return "Success salary/:id"
+    incSalary(@Param("id") id: number, @Query("increment") inc: number) {
+
     }
 
     @Put("active/:id")
-    toggleJobActiveStatus(
-        @Param("id", ParseIntPipe) id: number,
-        @Body("active", ParseBoolPipe) active: boolean
-    ) {
+    toggleJobActiveStatus(@Param("id") id: number, @Body("active") active: boolean) {
 
     }
 
     @Put("exp/:id")
-    setUpdateJobExp(@Param("id", ParseIntPipe) id: number, @Body("exp", new DefaultValuePipe(2), ParseIntPipe) exp: number) {
+    setUpdateJobExp(@Param("id") id: number, @Body("exp") exp: number) {
 
     }
 
     @Get("ref/:refId")
-    findJobByRefId(@Param("refId") id: string) {
+    findJobByRefId(@Param("refId", new ParseUUIDPipe({ version: '4'})) id: string) {
 
     }
 
     @Put("type/:id")
-    toggleJobType(@Param("id") id: number, @Body("type") type: JobType) {
+    toggleJobType(@Param("id") id: number, @Body("type", new ParseEnumPipe(JobType)) type: JobType) {
 
     }
 }
 
-// ParseIntPipe
-// ParseBoolPipe
-// new DefaultValuePipe(2) -> to set default value
+// ParseUUIDPipe -> Default version - 4
+// new ParseUUIDPipe({ version: '3'}) -> Set version to 3
+// new ParseEnumPipe(JobType) -> For enumerations
+// new ParseArrayPipe({ items: Number}) -> Approve array of numbers
+//  new ParseArrayPipe({ items: Number, separator: "-"}) -> Separator "-" only for http://localhost:3000/jobs/search/?id=10-2-1
