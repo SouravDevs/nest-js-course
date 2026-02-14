535a593d98bde4bc9837a222a7bff43bd728ba7e
SouravDevs <sourav2x4x@gmail.com>
2026-02-14 20:41:10 +0530

    #27- Exception Filters

--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -1,44 +1,17 @@
-import { Body, Controller, Get, Param, ParseArrayPipe, Post, Query, ValidationPipe } from "@nestjs/common";
+import { Controller, Get, NotFoundException, Param, ParseIntPipe } from "@nestjs/common";
 import { JobsService } from "./jobs.service";
-import { CreateJobDTO } from "./dto/create-jobs.dto";
-import { Paginable } from "./dto/paginable.dto";
 
 @Controller("jobs")
 export class JobsController {
     constructor(private jobsService: JobsService) { }
 
-    @Post()
-    // @UsePipes(ValidationPipe)
-    createJob(@Body(new ParseArrayPipe({ items: CreateJobDTO }), ValidationPipe) createJobDto: CreateJobDTO[]) {
-        return this.jobsService.createJob(createJobDto)
-    }
-
-    @Get("tags/:id")
-    findJobTags(@Query(new ValidationPipe({
-        transform: true,
-        // dismissDefaultMessages: true,
-        // disableErrorMessages: true ,
-        whitelist: true,
-        // skipMissingProperties: true,
-        stopAtFirstError: true
-    })) query: Paginable) {
-        console.log(query)
+    @Get(":id")
+    getJob(@Param("id", ParseIntPipe) id: number) {
+        if(id < 0) {
+            // throw new HttpException("Not Found", HttpStatus.NOT_FOUND)
+            throw new NotFoundException("Not Found")
+        }
         return { success: true }
     }
 
-    @Get(":id")
-    findJob(
-        @Param("id", new ValidationPipe({
-            transform: true,
-            // dismissDefaultMessages: true,
-            // disableErrorMessages: false,
-            // whitelist: true,
-            // skipMissingProperties: true,
-            // stopAtFirstError: true 
-        }))
-        id: number
-    ) {
-        console.log(typeof id)
-        return "success"
-    }
 }
