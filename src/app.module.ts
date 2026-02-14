a207d39a19bf3141652758e69ab8213062a99ac0
SouravDevs <sourav2x4x@gmail.com>
2026-02-14 19:42:25 +0530

    Clean Codes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,6 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
-import { AlbumsModule } from './albums/albums.module';
-import { JobsModule } from './jobs/jobs.module';
 
 @Module({
-  imports: [UsersModule, AlbumsModule, JobsModule]
+  imports: []
 })
 export class AppModule { }
