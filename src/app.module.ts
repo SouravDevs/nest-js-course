074059ccb9ec056517bd7baf061a9af5aa207449
SouravDevs <sourav2x4x@gmail.com>
2026-02-15 19:00:06 +0530

    Clean codes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
-import { JobsModule } from './jobs/jobs.module';
 
 @Module({
-  imports: [JobsModule]
+  imports: []
 })
 export class AppModule { }
--- src/jobs/jobs.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from "@nestjs/common";
-import { CreateJobDTO } from "./dto/create-jobs.dto";
-
-@Injectable()
-export class JobsService {
-    createJob(createJobDto: CreateJobDTO[]) {
-        return createJobDto
-    }
-}
