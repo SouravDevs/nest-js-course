520bf482ca65f43bb989b5c195b852b2250d80e5
SouravDevs <sourav2x4x@gmail.com>
2026-02-23 18:49:38 +0530

    #39 - MongoDB & Mongoose | Setup, Configuration Types & Connections

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,15 @@
 import { Module } from '@nestjs/common';
+import { JobsModule } from './jobs/jobs.module';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+
 @Module({
-  
+  imports: [
+    JobsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule
+  ]
 })
 export class AppModule { }
